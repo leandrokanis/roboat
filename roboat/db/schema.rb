439ec176e9c0ca627c0dbfa87b6a9d1e952dce80
Ruan Donato <ruandonato@outlook.com>
2017-04-14 09:56:35 -0300

    fixing error in relation boat and user

--- roboat/db/schema.rb
+++ roboat/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413232850) do
+ActiveRecord::Schema.define(version: 20170413231225) do
 
   create_table "boats", force: :cascade do |t|
     t.float    "battery_status"
@@ -18,15 +18,15 @@ ActiveRecord::Schema.define(version: 20170413232850) do
     t.float    "compass"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.integer  "boat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.index ["boat_id"], name: "index_users_on_boat_id"
   end
 
 end
