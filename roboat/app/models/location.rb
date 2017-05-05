5f8b8979ebc823e639f9f42d647f459d2ffe66e1
Ruan Donato <ruandonato@outlook.com>
2017-05-05 16:59:59 -0300

    adding latitute and longitude to form

--- roboat/app/models/location.rb
+++
@@ -1,5 +0,0 @@
-class Location < ApplicationRecord
-
-	
-
-end
--- roboat/config/routes.rb
+++ roboat/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :collects
   resources :measures
-  resources :locations
   resources :boats
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
