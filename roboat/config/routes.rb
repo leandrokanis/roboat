3bfeeef68ce98f84ad74576e87cc46522d099e50
Marcos Dourado <marcosdourado92@gmail.com>
2017-06-30 01:57:56 -0300

    changes route for sig in e sign up pages

--- roboat/config/routes.rb
+++ roboat/config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  get 'wellcome/index'
+
   devise_scope :user do
-    root to: "devise/sessions#new"
+    authenticated :user do
+      root 'collects#new', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
 
-  get 'wellcome/index'
   get 'collects/:id/receive_data_from_xbee' => 'collects#receive_data_from_xbee'
 
   resources :collects
